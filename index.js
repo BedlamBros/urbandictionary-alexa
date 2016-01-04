faf84c120a6c0539e2156ad0ee7e3391208271e5
Max McLennan <mpmclenn@indiana.edu>
2016-01-04 03:39:52 -0800

    Improved error handling, including timeout on AWS Lambda. Small degree of SSML enrichment for the example phrase.

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 var alexa = require('alexa-app'),
     _     = require('underscore'),
+    util  = require('util'),
     urban = require('urban'),
     ssml  = require('ssml');
 
-var APP_NAME = 'Urban Dictionary';
+    // application name
+var APP_NAME = 'Urban Dictionary',
+    // AWS Lambda timeout
+    TIMEOUT_MS = 1000,
+    // ms before TIMEOUT_MS when we consider this lambda to be 'timed out'
+    TIMEOUT_PREEMPTION_MS = 150,
+    // example Term arguments, specifically chosen because of their varying word count
+    EXAMPLE_TERMS = '{SJW|Donald Trump|Netflix and Chill|Term}';
 
 var app = new alexa.app(APP_NAME);
 
@@ -11,23 +19,59 @@ app.intent('DefineIntent', {
   'slots': {
     'Term': 'LITERAL'
   },
-  'utterances': [
-    'define {SJW|Donald Trump|Netflix and Chill|Term}'
-  ]
+  'utterances': _.map([
+    'define',
+    'define the word',
+    'define the term',
+    'define the phrase',
+    'explain',
+    'explain the word',
+    'explain the term',
+    'explain the phrase'
+      ], function(prefix) {
+        return prefix + ' ' + EXAMPLE_TERMS;
+    })
 }, function(req, res) {
   var term = req.slot('Term'),
       definition = null,
       example = null;
-  console.log('Defining term: ' + term);
 
+  if (typeof term === 'undefined') {
+    // no term was given
+    console.log('No term was given');
+    var errmsg = 'Sorry, I did not hear what term you wanted to define';
+    res.say(errmsg).card(APP_NAME, errmsg);
+    return true;
+  }
+
+  var TIMED_OUT = false;
+  var timeoutTimer = setTimeout(function() {
+    // declare the timeout
+    TIMED_OUT = true;
+    var errmsg = 'I had trouble communicating with urbandictionary.com';
+    // send the error message
+    res.say(errmsg).card(errmsg).send();
+  }, TIMEOUT_MS - TIMEOUT_PREEMPTION_MS);
+
+  console.log('Defining term: ' + term);
   urban(term).on('end', function(dictionary) {
+    // throw away this handler if timeout was already declared
+    if (TIMED_OUT) return;
+    var start = new Date();
+
+    // remove the timeout handler from the event queue
+    clearTimeout(timeoutTimer);
+
     if (dictionary.result_type == 'exact' && dictionary.list.length > 0) {
       // word was successfully defined
       var entry = dictionary.list[0];
-      definition = entry.definition;
-      example = entry.example;
-
-      res.say(new ssml().say(term).break(200));
+      var htmlRegex = /<(?:.|\n)*?>/gm;
+      definition = entry.definition.replace(htmlRegex, '');
+      if (!!entry.example) {
+        example = parseExample(entry.example.replace(htmlRegex, ''));
+      }
+      // state the term itself at beginning of response
+      res.say(new ssml().say(term).break(300));
     } else {
       definition = APP_NAME + ' could not find a definition for the term: ' + term;
     }
@@ -35,10 +79,12 @@ app.intent('DefineIntent', {
     res.say(definition);
     if (!!example) {
       // tack on an example usage if one was found
-      res.say(new ssml().break(200).say('Used in a sentence').break(200).say(example));
+      res.say(new ssml().break(500).say('Used in a sentence').break(300)).say(example);
     }
-    res.card(APP_NAME, definition);
-    res.send();
+    res.card(APP_NAME, definition).send();
+    console.log(util.format('Urban handler spent %d ms on CPU-bound work. ' +
+      'This should be less than %d.',
+      new Date().getTime() - start.getTime(), TIMEOUT_PREEMPTION_MS));
   });
 
   // return false to delay response
@@ -54,3 +100,21 @@ exports.schema = function() {
 exports.utterances = function() {
   console.log(app.utterances());
 };
+
+// convert an Urban Dictionary example phrase into ssml
+function parseExample(exampleParagraph) {
+  var example = new ssml();
+  var lines = exampleParagraph.split('\n');
+
+  for (var idx in lines) {
+    example = example.say(lines[idx]);
+    // do not add a breaks at end of the entire example
+    if (parseInt(idx) === lines.length - 1) continue;
+
+    // put breaks between 'Brian: ...\nJohn: ...\nBrian: ...' style conversations
+    if (lines[idx].match(/^[A-Za-z]+:/gi)) {
+      example = example.break(100);
+    }
+  }
+  return example;
+}
